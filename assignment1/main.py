e3de59fe769fc5f3fa3a9baaa12ce75f66594951
Sariah Shoemaker <sariah.shoemaker@du.edu>
2025-09-13 11:38:52 -0600

    assignment 1 submission

---
+++ assignment1/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("To learn how the internet works")
+    print("To get a skill that will help me get a job")
+    print("To learn to use Wireshark more effectively")
+
+if __name__=="__main__":
+    main()
